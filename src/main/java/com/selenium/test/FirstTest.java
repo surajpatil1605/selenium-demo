f43fda552a7a5f2ef5796e37fdfbe707952c942f
Suraj <suraj.patil@wolterskluwer.com>
2023-09-22 17:03:44 +0530

    First commit

---
+++ src/main/java/com/selenium/test/FirstTest.java
@@ -0,0 +1,41 @@
+package com.selenium.test;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void testComponents() {
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Add this line to run in headless mode
+		WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        driver.quit();
+	}
+
+}
